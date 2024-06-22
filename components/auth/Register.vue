431f0bc773ecefee1de64a0be883886ed946d882
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-06-22 22:31:46 +0300

    update register

--- components/auth/Register.vue
+++ components/auth/Register.vue
@@ -183,7 +183,7 @@
     <button
       type="submit"
       @click.prevent="rewgister"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:border-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:border-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
     >
       Submit
     </button>
