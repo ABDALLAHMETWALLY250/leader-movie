09fb963cf12f685510dd60c6026666d65cb4a239
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-06-27 00:03:44 +0300

    update  middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,12 @@ export default defineNuxtRouteMiddleware((to) => {
   const token = useCookie("token");
   if (!token.value && to.path == "/") {
     return navigateTo("/auth/login");
-  } else {
+  }
+
+  if (
+    (to.path == "/auth/login" || to.path == "/auth/register") &&
+    token.value
+  ) {
     return navigateTo("/");
   }
 });
