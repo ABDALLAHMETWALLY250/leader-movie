545c44b78dab267363d5f0ceafc384d2a24ebb11
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-06-29 23:58:27 +0300

    update login and register layout and home layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,10 @@
 <template >
   <div class="container mx-auto">
-    <h1 class="text-3xl text-red-600">hello</h1>
-
-    <p>count {{ countStore.count }}</p>
-
-    <button
-      class="bg-sky-500 text-white px-8 py-2 rounded-md"
-      @click="countStore.increment"
-    >
-      button
-    </button>
+    <h1>adasd</h1>
   </div>
 </template>
 <script setup>
-import { useCountStore } from "../stores/count";
-definePageMeta({
-  middleware: "auth", // this should match the name of the file inside the middleware directory
-});
-const countStore = useCountStore();
+// definePageMeta({
+//   middleware: "auth", // this should match the name of the file inside the middleware directory
+// });
 </script>
-<style lang="">
-</style>
