cbe188b214a65a87a6fa624376038b66186be588
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-08-27 03:15:15 +0300

    Update AllMovies.ts

--- stores/Movies/AllMovies.ts
+++ stores/Movies/AllMovies.ts
@@ -3,10 +3,10 @@ export const useAllMoviesStore = defineStore("allMovies", {
     allMovies: [],
   }),
   actions: {
-    setAllMovies(language: any) {
+   async setAllMovies(language: any) {
       const api_key = "e62b5c7ac206f4ba1f5625e1433cef42";
 
-      fetch(
+   await fetch(
         `https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=${language}&page=1&sort_by=popularity.desc&api_key=${api_key}`
       ).then((res) =>
         res.json().then((data) => {
