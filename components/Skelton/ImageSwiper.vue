3a25bb0558a459ecf846c6020d6596044dc47227
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-09-08 19:36:00 +0300

    update image Swiper

---
+++ components/Skelton/ImageSwiper.vue
@@ -0,0 +1,22 @@
+<template >
+  <div>
+    <div class="px-4">
+      <Swiper
+        class="mySwiper xl:container xl:mx-auto lg:container lg:mx-auto"
+        :modules="[SwiperAutoplay, SwiperPagination]"
+        :slides-per-view="1"
+        :loop="true"
+        :pagination="{
+          clickable: true,
+        }"
+        :autoplay="{
+          delay: 4000,
+        }"
+      >
+        <SwiperSlide v-for="slide in 6" :key="slide">
+          <Skeleton width="100%" height="350px"></Skeleton>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </div>
+</template>
