a135148c73b0f79a3793ad50f3835ad8868ea24a
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-09-09 16:21:01 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -6,23 +6,29 @@
         <NuxtPage />
       </div>
       <div class="w-full h-screen flex items-center justify-center text-3xl font-bold" v-else>
-      Loading ....
+        Loading ....
       </div>
     </NuxtLayout>
   </div>
 </template>
 
-
 <script setup lang="ts">
-  const loading  = ref(true)
+import { ref, onMounted } from 'vue';
+
+const loading = ref(true);
 const newTheme = ref<string>("");
 
 const changeTheme = (Theme: string) => {
   newTheme.value = Theme;
 };
+
 onMounted(() => {
   newTheme.value = localStorage.getItem("theme") || "light";
   console.log("loading");
-   setTimeout(loading.value = false, 5000);
+  
+  // Set the timeout to update the loading state after 5 seconds
+  setTimeout(() => {
+    loading.value = false;
+  }, 5000);
 });
 </script>
