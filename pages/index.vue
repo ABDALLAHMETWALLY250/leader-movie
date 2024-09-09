54dfec3bc55ba2504359deef49abfbd50275f051
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-09-09 16:28:08 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,15 @@
   const loading = ref(false)
 
 
-
+onMounted(() => {
+  newTheme.value = localStorage.getItem("theme") || "light";
+  console.log("loading");
+  
+  // Set the timeout to update the loading state after 5 seconds
+  setTimeout(() => {
+    loading.value = false;
+  }, 5000);
+});
   
 </script>
 
