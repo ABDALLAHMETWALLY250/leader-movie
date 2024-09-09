cf7157ef027414377a3edea237a36fd83cdd5924
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-09-09 16:27:40 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <ImageSwiperImages />
+ <div v-if="loading == false">
+
+     <ImageSwiperImages />
 
   <HomeMovie />
   <div class="px-10">
     <div class="border_top w-full"></div>
   </div>
   <TvShowTvshow />
+ </div>
 </template>
 <script setup lang="ts">
+
+  const loading = ref(false)
+
+
+
+  
 </script>
 
