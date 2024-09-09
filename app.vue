3989836d9604f157ec84c349f914ef07503c2539
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-09-09 16:29:23 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,21 +1,13 @@
 <template>
-  <div :id="newTheme" :dir="$i18n.locale == 'ar' ? 'rtl' : 'ltr'" v-if="!loading">
+  <div :id="newTheme" :dir="$i18n.locale == 'ar' ? 'rtl' : 'ltr'">
     <NuxtRouteAnnouncer />
     <NuxtLayout @changeTheme="changeTheme">
-      <div >
         <NuxtPage />
-      </div>
-      <div class="w-full h-screen flex items-center justify-center text-3xl font-bold" v-else>
-        Loading ....
-      </div>
     </NuxtLayout>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-const loading = ref(true);
 const newTheme = ref<string>("");
 
 const changeTheme = (Theme: string) => {
@@ -24,11 +16,5 @@ const changeTheme = (Theme: string) => {
 
 onMounted(() => {
   newTheme.value = localStorage.getItem("theme") || "light";
-  console.log("loading");
-  
-  // Set the timeout to update the loading state after 5 seconds
-  setTimeout(() => {
-    loading.value = false;
-  }, 5000);
 });
 </script>
