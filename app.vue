70e81e313e3b855f60395617df49ba65c234f180
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-09-13 23:48:58 +0300

    fix bugs

--- app.vue
+++ app.vue
@@ -1,12 +1,3 @@
-<template>
-  <div :id="newTheme" :dir="$i18n.locale == 'ar' ? 'rtl' : 'ltr'">
-    <NuxtRouteAnnouncer />
-    <NuxtLayout @changeTheme="changeTheme">
-        <NuxtPage />
-    </NuxtLayout>
-  </div>
-</template>
-
 <script setup lang="ts">
 const newTheme = ref<string>("");
 
@@ -18,3 +9,14 @@ onMounted(() => {
   newTheme.value = localStorage.getItem("theme") || "light";
 });
 </script>
+
+
+<template>
+  <div :id="newTheme" :dir="$i18n.locale == 'ar' ? 'rtl' : 'ltr'">
+    <NuxtRouteAnnouncer />
+    <NuxtLayout @changeTheme="changeTheme">
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
