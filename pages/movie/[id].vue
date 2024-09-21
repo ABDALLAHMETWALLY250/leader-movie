63ba7edfcc69633a0ab3e5a93d0638527d00d379
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2024-09-22 02:21:18 +0300

    fix bugs

--- pages/movie/[id].vue
+++ pages/movie/[id].vue
@@ -1,11 +1,26 @@
+<script setup lang="ts">
+import { useMovieDetails } from "~/stores/movieDetails/movieDetails";
+
+const movieDetails = useMovieDetails();
+
+const { locale } = useI18n();
+
+const route = useRoute();
+
+const fetchMovieDetails = () => {
+  movieDetails.getMovieDetails(route.params.id, locale.value);
+};
+
+onMounted(() => {
+  fetchMovieDetails();
+});
+</script>
+
+
 <template >
   <div>
-    <h1>{{ Route.params.id }}</h1>
+    <h1>{{ route.params.id }}</h1>
+    {{ movieDetails.movieDetails.homepage }}
+    {{ movieDetails.movieDetails.overview }}
   </div>
 </template>
-<script setup lang="ts">
-const { locale } = useI18n();
-const Route = useRoute();
-</script>
-<style lang="">
-</style>
